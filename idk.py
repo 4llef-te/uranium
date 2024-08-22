8bdc0b862a26fd776e361c1e27918b90d725c964
4llef-te <123855501+4llef-te@users.noreply.github.com>
2024-08-22 14:27:48 +0330

    first dop not even alpha

--- idk.py
+++ idk.py
@@ -1,7 +1,3 @@
 import freecurrencyapi
 client = freecurrencyapi.Client('fca_live_cMbpMtpIAOEQJxWJpx2xKALmtjgfHjdlxHDZEqcL')
 result = client.latest('CAD')
-print(result)
-print(result)
-print(result)
-print(result)
